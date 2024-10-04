7c5a35d98de693d6f40d876e96910b4ca25d7d58
SvetucClub <artur_rumyantsev@inbox.ru>
2024-10-04 18:47:05 +0300

    void-92 progress v0.2.1

---
+++ Assets/Post-Process Anims/Glitch0.cs
@@ -0,0 +1,17 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Glitch0 : MonoBehaviour
+{
+    public Volume GlobalVolume;
+    private void Start()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GlobalVolume.profile.
+    }
+}
